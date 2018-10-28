a6915885bf2bf75f2becb5d29dfa72975b3c7a8c
babind <babin.dahal3@gmail.com>
2018-10-28 11:00:08 +1100

    making partials

--- app/Question.php
+++ app/Question.php
@@ -49,7 +49,7 @@ class Question extends Model
 
     public function answers()
     {
-        return $this->hasMany(Answer::class);
+        return $this->hasMany(Answer::class)->orderBy('votes_count', 'DESC');
     }
 
     public function acceptBestAnswer(Answer $answer)
@@ -67,7 +67,7 @@ class Question extends Model
     {
         return str_limit(strip_tags($this->bodyHtml()), $length);
     }
-    
+
     private function bodyHtml()
     {
         return \Parsedown::instance()->text($this->body);
